66f46c8f4f404514df83f69270a51a16ab5ed7a4
krisvesely <kristine.vesely@gmail.com>
2023-06-21 16:25:30 -0400

    create ng auth service, full-stack test

---
+++ ngPawrentsPlace/src/app/models/user.ts
@@ -0,0 +1,27 @@
+export class User {
+
+
+  id: number;
+  // email: string;
+  username: string;
+  password: string;
+  enabled: boolean;
+  role: string;
+
+  constructor(
+  id: number = 0,
+  // email: string = '',
+  username: string = '',
+  password: string = '',
+  enabled: boolean = false,
+  role: string = ''
+  ) {
+    this.id = id;
+    // this.email = email;
+    this.username = username;
+    this.password = password;
+    this.enabled = enabled;
+    this.role = role;
+  }
+
+}

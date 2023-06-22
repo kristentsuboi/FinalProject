9a19f7394262c40f0327b33b6437858a101880dd
mannygutierrez12 <iberiard@gmail.com>
2023-06-22 12:44:07 -0400

    Completed remaining entities

---
+++ JPAPawrentsPlace/src/test/java/com/skilldistillery/pawrentsplace/entities/AddressTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.pawrentsplace.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AddressTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Address Address;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAPawrentsPlace");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		Address = em.find(Address.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		Address = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(Address);
+	}
+
+}

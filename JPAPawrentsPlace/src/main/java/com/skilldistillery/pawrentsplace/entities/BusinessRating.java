d198c0ab5e65822263f8021d2d3a775df5b52522
krisvesely <kristine.vesely@gmail.com>
2023-06-22 15:55:45 -0400

    build ServiceType, BusinessRating(Id) entities and tests

---
+++ JPAPawrentsPlace/src/main/java/com/skilldistillery/pawrentsplace/entities/BusinessRating.java
@@ -0,0 +1,101 @@
+package com.skilldistillery.pawrentsplace.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="business_rating")
+public class BusinessRating {
+
+	@EmbeddedId
+	private BusinessRatingId id;
+	
+	private int rating;
+	
+	private String remark;
+	
+	@Column(name = "created_at")
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@Column(name = "updated_at")
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+	public BusinessRating() {
+		super();
+	}
+
+	public BusinessRatingId getId() {
+		return id;
+	}
+
+	public void setId(BusinessRatingId id) {
+		this.id = id;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@Override
+	public String toString() {
+		return "BusinessRating [id=" + id + ", rating=" + rating + ", remark=" + remark + ", createdAt=" + createdAt
+				+ ", updatedAt=" + updatedAt + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BusinessRating other = (BusinessRating) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}

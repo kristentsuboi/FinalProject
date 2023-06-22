75c81eb691e38efcfd89b987fedf211458b33b26
mannygutierrez12 <iberiard@gmail.com>
2023-06-22 15:58:06 -0400

    created the inserts

--- DB/petdb.sql
+++ DB/petdb.sql
@@ -44,11 +44,52 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
+<<<<<<< Updated upstream
+=======
+-- Data for table `address`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `petdb`;
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (1, '123 Main St', 'Anycity', 'Colorado', '23948');
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (2, '456 Main St', 'Fayetteville', 'North Carolina', '28311');
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (3, '123 Owner st', 'Fayetteville', 'North Carolina', '28304');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `business`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `petdb`;
+INSERT INTO `business` (`id`, `name`, `about`, `phone`, `image_url`, `address_id`) VALUES (1, 'fantastic pets', 'certified local vet', '5555555555', NULL, 2);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+>>>>>>> Stashed changes
 -- Data for table `user`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `petdb`;
+<<<<<<< Updated upstream
 INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (1, 'admin', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, 'admin');
+=======
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `email`, `phone`, `image_url`, `created_at`, `updated_at`, `business_id`, `address_id`) VALUES (1, 'admin', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, 'admin', 'admin@email.com', '123456789', NULL, NULL, NULL, NULL, 1);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `email`, `phone`, `image_url`, `created_at`, `updated_at`, `business_id`, `address_id`) VALUES (2, 'vet', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, 'vet', 'vet@email.com', '345234234', NULL, NULL, NULL, 1, 2);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `email`, `phone`, `image_url`, `created_at`, `updated_at`, `business_id`, `address_id`) VALUES (3, 'petowner', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', 1, 'pawrent', 'owner@email.com', '3214568790', NULL, NULL, NULL, NULL, 3);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `pet`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `petdb`;
+INSERT INTO `pet` (`id`, `name`, `species`, `breed`, `height`, `weight`, `gender`, `birth`, `color`, `about`, `microchipped`, `image_url`, `allergies`, `enabled`, `created_at`, `updated_at`, `owner_id`) VALUES (1, 'brandon', 'dog', 'chihuahua', 1, 6, 'male', '2018-12-21', 'brown', 'small dog', 0, NULL, 'none', 1, NULL, NULL, 3);
+>>>>>>> Stashed changes
 
 COMMIT;
 

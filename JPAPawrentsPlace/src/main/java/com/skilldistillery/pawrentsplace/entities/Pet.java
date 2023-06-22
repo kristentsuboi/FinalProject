a118bee1c1dc38217b3a12c2553b4ea4e7349e22
krisvesely <kristine.vesely@gmail.com>
2023-06-22 16:29:46 -0400

    added generated value to pet entity

--- JPAPawrentsPlace/src/main/java/com/skilldistillery/pawrentsplace/entities/Pet.java
+++ JPAPawrentsPlace/src/main/java/com/skilldistillery/pawrentsplace/entities/Pet.java
@@ -4,10 +4,15 @@ import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Pet {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	private String name;

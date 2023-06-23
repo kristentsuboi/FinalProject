eea1cf661cd6aed13dd5ec1a56043ce4ead3a727
krisvesely <kristine.vesely@gmail.com>
2023-06-23 13:59:20 -0400

    create singular Pet Comp; build out Diet model

--- ngPawrentsPlace/src/app/models/diet.ts
+++ ngPawrentsPlace/src/app/models/diet.ts
@@ -1,2 +1,30 @@
+import { Pet } from "./pet";
+
 export class Diet {
+
+  id: number;
+  name: string;
+  type: string;
+  frequency: string;
+  notes: string;
+  amount: string;
+  pet: Pet | null;
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    type: string = '',
+    frequency: string = '',
+    notes: string = '',
+    amount: string = '',
+    pet: Pet | null = null
+  ) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.frequency = frequency;
+    this.notes = notes;
+    this.amount = amount;
+    this.pet = pet;
+  }
 }

673c7617ba093130434c29093d5179e1524c34b1
krisvesely <kristine.vesely@gmail.com>
2023-06-23 12:43:51 -0400

    biuld out PetList Comp TS

--- ngPawrentsPlace/src/app/components/pet-list/pet-list.component.ts
+++ ngPawrentsPlace/src/app/components/pet-list/pet-list.component.ts
@@ -57,6 +57,68 @@ export class PetListComponent {
     });
   }
 
+    addTodo(newPet: Pet): void {
+    console.log(newPet);
+    this.petService.create(newPet).subscribe({
+      next: (result) => {
+        this.newPet = new Pet();
+        this.reload();
+        this.selected = result;
+      },
+      error: (nojoy) => {
+        console.error('PetListHttpComponent.addTodo(): error creating pet:');
+        console.error(nojoy);
+      },
+    });
 
+  }
+
+  setEditPet() {
+    this.editPet = Object.assign({}, this.selected);
+  }
+
+  cancelEdit() {
+    this.editPet = null;
+  }
+
+  updatePet(editPet: Pet): void {
+    console.log(editPet);
+    this.petService.update(editPet).subscribe({
+      next: (result) => {
+        this.editPet = null;
+        this.reload();
+        this.selected = result;
+      },
+      error: (nojoy) => {
+        console.error(
+          'PetListHttpComponent.updateTodo(): error updating pet:'
+        );
+        console.error(nojoy);
+      },
+    });
+  }
+displayPet(selectedPet: Pet) {
+    this.router.navigateByUrl('pets/' + selectedPet.id);
+    this.selected = selectedPet;
+  }
+
+  displayTable() {
+    this.router.navigateByUrl('pets');
+    this.selected = null;
+  }
+
+  deletePet(id: number) {
+    this.petService.destroy(id).subscribe({
+      next: (result) => {
+        this.reload();
+      },
+      error: (nojoy) => {
+        console.error(
+          'PetListHttpComponent.deleteTodo(): error deleting pet:'
+        );
+        console.error(nojoy);
+      },
+    });
+  }
 
 }

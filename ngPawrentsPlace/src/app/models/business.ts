9cadd6f891dcb746d23fdc1b77074e6bea3f3c18
kristentsuboi <126204286+kristentsuboi@users.noreply.github.com>
2023-06-23 20:10:06 -0400

    added model information

--- ngPawrentsPlace/src/app/models/business.ts
+++ ngPawrentsPlace/src/app/models/business.ts
@@ -1,2 +1,21 @@
 export class Business {
+  id: number;
+  name: string;
+  about: string;
+  phone: string;
+  imageUrl: string;
+
+  constructor(
+    id: number = 0,
+  name: string = "",
+  about: string = "",
+  phone: string = "",
+  imageUrl: string = ""
+  ){
+    this.id = id;
+    this.name = name;
+    this.about = about;
+    this.phone = phone;
+    this.imageUrl = imageUrl
+  }
 }
--- ngPawrentsPlace/src/app/models/medical-note.ts
+++ ngPawrentsPlace/src/app/models/medical-note.ts
@@ -1,2 +1,19 @@
 export class MedicalNote {
+  id: number;
+  notes: string;
+  createAt: string;
+  updatedAt: string;
+
+  constructor(
+    id: number = 0,
+  notes: string = "",
+  createAt: string = "",
+  updatedAt: string =  ""
+  ){
+    this.id = id;
+    this.notes = notes;
+    this.createAt = createAt;
+    this.updatedAt = updatedAt;
+  }
+
 }

7283bb0b48428e4cfc4c2c877d25a1569f6fc71d
kristentsuboi <126204286+kristentsuboi@users.noreply.github.com>
2023-06-26 18:41:09 -0400

    stubbed biz

--- ngPawrentsPlace/src/app/components/business/business.component.ts
+++ ngPawrentsPlace/src/app/components/business/business.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Business } from 'src/app/models/business';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-business',
@@ -6,5 +10,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./business.component.css']
 })
 export class BusinessComponent {
+
+  business: Business | null = null;
+  loggedInUser: User | null = null;
+
+  constructor(private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute){}
+
+
+    ngOnInit(){
+      this.getLoggedInUser();
+      this.getBusiness();
+    }
+
+    getLoggedInUser() {
+      this.authService.getLoggedInUser().subscribe({
+        next: (user) => {
+          console.log(user);
+          this.loggedInUser = user;
+        },
+        error: function (theError) {
+          console.error('BusinessComponent.reload(): Error loading user.');
+          console.error(theError);
+        },
+      });
+    }
+
+
+
+
+
+
 
 }

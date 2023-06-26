1f235997342f35d99c25ece96004ab18ba1176ed
krisvesely <kristine.vesely@gmail.com>
2023-06-26 15:12:27 -0400

    create service-type controller and serviceimpl with index method

--- PawrentsPlace/src/main/java/com/skilldistillery/pawrentsplace/services/ServiceTypeService.java
+++ PawrentsPlace/src/main/java/com/skilldistillery/pawrentsplace/services/ServiceTypeService.java
@@ -1,6 +1,11 @@
 package com.skilldistillery.pawrentsplace.services;
 
+import java.util.List;
+
+import com.skilldistillery.pawrentsplace.entities.ServiceType;
+
 public interface ServiceTypeService {
 	
+	List<ServiceType> index();
 
 }

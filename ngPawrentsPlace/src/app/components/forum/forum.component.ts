10ab7402cbce99356312cb0b7856a7d347b0c653
mannygutierrez12 <iberiard@gmail.com>
2023-06-26 18:55:33 -0400

    created the service for comment to create the forum component

--- ngPawrentsPlace/src/app/components/forum/forum.component.ts
+++ ngPawrentsPlace/src/app/components/forum/forum.component.ts
@@ -1,10 +1,62 @@
+import { CommentService } from './../../services/comment.service';
 import { Component } from '@angular/core';
+import { Comment } from './../../models/comment';
 
 @Component({
   selector: 'app-forum',
   templateUrl: './forum.component.html',
-  styleUrls: ['./forum.component.css']
+  styleUrls: ['./forum.component.css'],
 })
 export class ForumComponent {
+  comments: Comment[] = [];
+  selected: Comment | null = null;
+  selectedType: string = 'all';
+  newComment: Comment = new Comment();
 
+  constructor(private commentService: CommentService) {}
+
+  ngOnInit(): void {
+    this.loadComments();
+  }
+
+  types = [
+    'Health and Wellness',
+    'Training',
+    'Adoption and Rescue',
+    'Pet Loss Support',
+    'Traveling with Pets',
+    'Pet Stories',
+    'Pet Meme',
+    'Pet Photography',
+    'Grooming',
+    'Kids and Pets',
+  ];
+
+  loadComments(): void {
+    this.commentService.index().subscribe({
+      next: (commentList) => {
+        this.comments = commentList;
+      },
+      error: (error) => {
+        console.error(
+          'ForumComponent.loadComments(): Error loading comments list'
+        );
+        console.error(error);
+      },
+    });
+  }
+
+  addComment(newComment: Comment): void {
+    console.log(newComment);
+    this.commentService.create(newComment).subscribe({
+      next: (createdComment) => {
+        this.loadComments();
+        this.selected = createdComment;
+      },
+      error: (error) => {
+        console.error('ForumComponent.addComment(): Error creating comment');
+        console.error(error);
+      },
+    });
+  }
 }
--- ngPawrentsPlace/src/app/services/comment.service.ts
+++ ngPawrentsPlace/src/app/services/comment.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Comment } from '../models/comment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
+  url = 'http://api.skilldistillery.com:8080/comments/data/comment';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  index(): Observable<Comment[]> {
+    return this.http.get<Comment[]>(this.url + '?sorted=true').pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          new Error('CommentService.index(): error retrieving comments: ' + err)
+        );
+      })
+    );
+  }
+
+  create(comment: Comment): Observable<Comment> {
+    return this.http.post<Comment>(this.url, comment).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('CommentService.create(): error creating comment')
+        );
+      })
+    );
+  }
 }

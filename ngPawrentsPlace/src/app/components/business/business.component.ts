bf38e5e040a031c7fab5d9f3858790c1cbb659fb
kristentsuboi <126204286+kristentsuboi@users.noreply.github.com>
2023-06-26 19:12:47 -0400

    business component stubbed

--- ngPawrentsPlace/src/app/components/business/business.component.ts
+++ ngPawrentsPlace/src/app/components/business/business.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Business } from 'src/app/models/business';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { BusinessService } from 'src/app/services/business.service';
 
 @Component({
   selector: 'app-business',
@@ -11,17 +12,39 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class BusinessComponent {
 
-  business: Business | null = null;
+  selected: Business | null = null;
   loggedInUser: User | null = null;
 
   constructor(private authService: AuthService,
     private router: Router,
-    private route: ActivatedRoute){}
+    private route: ActivatedRoute,
+    private businessService: BusinessService){}
 
 
-    ngOnInit(){
-      this.getLoggedInUser();
-      this.getBusiness();
+
+    ngOnInit(): void {
+
+      let idStr = this.route.snapshot.paramMap.get('id');
+      if (idStr) {
+        let id: number = Number.parseInt(idStr);
+        if (isNaN(id)) {
+          this.router.navigateByUrl('**');
+        } else {
+          this.businessService.show(id).subscribe({
+            next: (business) => {
+              this.getLoggedInUser();
+              this.selected = business;
+            },
+            error: (theError) => {
+              console.error('BusinessComponent.ngOnInit(): Error loading pet.');
+              console.error(theError);
+              this.router.navigateByUrl('**');
+            },
+          });
+        }
+      } else {
+        this.router.navigateByUrl('home');
+      }
     }
 
     getLoggedInUser() {
@@ -37,6 +60,17 @@ export class BusinessComponent {
       });
     }
 
+    getBusiness(businessId: number){
+      this.businessService.show(businessId).subscribe({
+        next: (business) => {
+          this.selected = business;
+        },
+        error: function (theError) {
+          console.error('PetComponent.reload(): Error loading pet.');
+          console.error(theError);
+        }
+      })
+    }
 
 
 

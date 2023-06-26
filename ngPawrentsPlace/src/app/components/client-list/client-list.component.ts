840c58652424e04cd2769f9e938537e8bf946666
kristentsuboi <126204286+kristentsuboi@users.noreply.github.com>
2023-06-26 15:42:22 -0400

    no changes. switching drivers

--- ngPawrentsPlace/src/app/components/client-list/client-list.component.ts
+++ ngPawrentsPlace/src/app/components/client-list/client-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-client-list',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./client-list.component.css']
 })
 export class ClientListComponent {
+  clients: User[] = [];
+  bizOwner: User | null = null;
+
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
 }
